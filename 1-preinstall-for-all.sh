a0f6f16c6eba1ca110488fe038fef98dd5d8ed14
伍育珂 <yorkwu@digitalgd.com.cn>
2019-06-27 18:03:57 +0800

    no message

--- 1-preinstall-for-all.sh
+++ 1-preinstall-for-all.sh
@@ -31,7 +31,7 @@ yum install -y ntpdate
 ntpdate -u ntp.api.bz
 
 # 确认内核版本后，开启IPVS
-
+uname -a
 cat > /etc/sysconfig/modules/ipvs.modules <<EOF
 #!/bin/bash
 ipvs_modules="ip_vs ip_vs_lc ip_vs_wlc ip_vs_rr ip_vs_wrr ip_vs_lblc ip_vs_lblcr ip_vs_dh ip_vs_sh ip_vs_fo ip_vs_nq ip_vs_sed ip_vs_ftp nf_conntrack"
@@ -42,5 +42,4 @@ for kernel_module in \${ipvs_modules}; do
   fi
 done
 EOF
-
 chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep ip_vs
